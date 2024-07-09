5512e501905fa26acf580ec2b79ec4aea8e6d965
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2024-07-09 12:17:16 +0700

    add components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Head from "@/components/head"
+
 
 export default function Home() {
   return (
     <main className="">
-
+        <Head/>
     </main>
   );
 }
---
+++ src/components/head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Head = () => {
+  return (
+    <div className='w-full h-screen bg-white'>
+        <div className='font-extrabold text-5xl max-w-[1000px] mx-auto py-12 items-center justify-center'>
+            Data Structure Viusal
+        </div>
+    </div>
+  )
+}
