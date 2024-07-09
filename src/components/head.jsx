c3e6688af7d1bcb6f025695dd76d220cdc818204
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2024-07-10 01:48:04 +0700

    edit head

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -4,7 +4,7 @@ const Head = () => {
   return (
     <div className='w-full h-screen bg-white'>
         <div className='font-extrabold text-5xl max-w-[1000px] mx-auto py-12 justify-center flex text-black'>
-            Data Structure Viusal
+            Data Structure Viusalizer
         </div>
     </div>
   )
