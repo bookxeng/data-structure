f8edcd65b321d36c837309758b86b63d17657e20
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2024-07-09 12:52:43 +0700

    add list and change head

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export const Head = () => {
+const Head = () => {
   return (
     <div className='w-full h-screen bg-white'>
-        <div className='font-extrabold text-5xl max-w-[1000px] mx-auto py-12 items-center justify-center'>
+        <div className='font-extrabold text-5xl max-w-[1000px] mx-auto py-12 justify-center flex text-black'>
             Data Structure Viusal
         </div>
     </div>
   )
 }
+
+export default Head;
---
+++ src/components/list.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div className='w-full min-h-screen bg-white'>
+        <div className='max-w-[1000px] py-12 mx-auto text-black text-2xl items-center justify-start'>
+            <ul className='flex-col flex'>
+                <li>
+                    Array
+                </li>
+                <li>
+                    Linked List
+                </li>
+                <li>
+                    tree
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default List;
