09c032fbb2461bf2005165dfff245b10c182c5d8
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2024-07-14 16:01:47 +0700

    add new pages

--- src/app/linked-list/LinkedListFunc.jsx
+++ src/app/linked-list/LinkedListFunc.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Array ()  {
+export default function Linked ()  {
   return (
     <div className='w-full min-h-screen bg-white'>
         <div className='text-black text-5xl font-extrabold max-w-[1000px] mx-auto py-12 justify-center flex'>
-            Array
+            Linked List
         </div>
     </div>
   )
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -12,15 +12,19 @@ const List = () => {
         <div className='font-extrabold max-w-[1000px] py-12 mx-auto text-black text-2xl items-center justify-start'>
             <ul className='flex-col flex'>
                 <li>
-                    <Link href="/array" className=''>
+                    <Link href="/array" className='text-black hover:text-blue-500'>
                         Array
                     </Link>
                 </li>
                 <li>
-                    Linked List
+                    <Link href="/linked-list" className='text-black hover:text-blue-500'>
+                        Linked List
+                    </Link>
                 </li>
                 <li>
-                    tree
+                    <Link href="/tree" className='text-black hover:text-blue-500'>
+                        Tree
+                    </Link>
                 </li>
             </ul>
         </div>
