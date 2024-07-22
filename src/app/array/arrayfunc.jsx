9468a4cd4f72e4c3bc10df44c176ef251052fde8
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2024-07-22 23:32:24 +0700

    add table to array

--- src/app/array/arrayfunc.jsx
+++ src/app/array/arrayfunc.jsx
@@ -6,6 +6,17 @@ export default function Array ()  {
         <div className='text-black text-5xl font-extrabold max-w-[1000px] mx-auto py-12 justify-center flex'>
             Array
         </div>
+        <div className='text-center justify-center flex'>
+          <table className='border-separate border-black border-4 border-spacing-2'>
+            <tbody>
+            <tr className='text-4xl font-extrabold'>
+              <td className=' border-black border-2'>1</td>
+              <td className=' border-black border-2'>2</td>
+              <td className=' border-black border-2'>200</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
     </div>
   )
 }
