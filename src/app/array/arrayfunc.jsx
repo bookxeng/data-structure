12cb3815b29fc8194f09559b0c6816af2832654d
Pongnapat L <pongnapatlimmongkolhirun@gmail.com>
2024-07-24 08:09:55 +0700

    add text form

--- src/app/array/arrayfunc.jsx
+++ src/app/array/arrayfunc.jsx
@@ -6,10 +6,14 @@ export default function Array ()  {
         <div className='text-black text-5xl font-extrabold max-w-[1000px] mx-auto py-12 justify-center flex'>
             Array
         </div>
-        <div className='text-center justify-center flex'>
+        <div className='max-w-[1000px] mx-auto py-12 justify-start flex text-black'>
+          Type an index for array
+          <input type='text' className='form-text border-black border-2 text-black mx-2'/>
+        </div>
+        <div className='text-center justify-center flex py-12'>
           <table className='border-separate border-black border-4 border-spacing-2'>
             <tbody>
-            <tr className='text-4xl font-extrabold'>
+            <tr className='text-4xl font-extrabold text-black'>
               <td className=' border-black border-2'>1</td>
               <td className=' border-black border-2'>2</td>
               <td className=' border-black border-2'>200</td>
